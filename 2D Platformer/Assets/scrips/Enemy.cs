2e19527160ff8193624d82bd166fdd88911d4d55
masons002 <mason.s002@stu.nebo.edu>
2025-10-27 13:38:46 -0600

    added sprite

--- 2D Platformer/Assets/scrips/Enemy.cs
+++ 2D Platformer/Assets/scrips/Enemy.cs
@@ -1,7 +1,6 @@
-using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-
+using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
@@ -10,8 +9,10 @@ public class Enemy : MonoBehaviour
     public Vector3 moveOffset;
     private Vector3 startPos;
     private Vector3 targetPos;
-    
-    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+
+
     void Start()
     {
         startPos = transform.position;
@@ -21,6 +22,47 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.DeltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+
+        if(transform.position == startPos)
+        {
+            targetPos = startPos + moveOffset;
+        }
+
+        else
+        {
+            targetPos = startPos;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) 
+
+    {
+        if(collision.CompareTag("Player"))
+        {
+            collision.GetComponent<PlayerController2D>().GameOver();
+        }
+    }
+    
+    private void OnDrawGizmos();
+    {
+        Vector3 from;
+        Vector3 to;
+
+        if(Application.isPlaying)
+        {
+            from = startPos;
+        }
+        else
+        {
+            from = transform.position;
+        }
+
+        to = from + moveOffset;
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(from, to);
+        Gizmos.DrawWireSphere(to, 0.2f);
+        Gizmos.DrawWireSphere(from, 0.2f);
     }
 }
